6d767c88e0c27552a912baeab30b8738b4b00f36
Brecht Tourlousse <brecht.tourlousse@studiohyperdrive.be>
2021-02-21 20:44:36 +0100

    feat: add swagger + example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  @ApiQuery({
+    name: 'contentType',
+    type: 'enum',
+    required: false,
+    enum: ['Serial'],
+  })
   getHello(): string {
     return this.appService.getHello();
   }
